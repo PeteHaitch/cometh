94e2db00d0fd3f024eea4ef33ed6aff23d96ab1d
Peter Hickey <peter.hickey@gmail.com>
2014-06-25 20:31:48 +1000

    Complete re-write of read.comethlyation to use data.table for reading and merging of comethylation .tsv files.

--- R/read.comethylation.R
+++ R/read.comethylation.R
@@ -2,7 +2,7 @@
 ## TODO: Add bzip support.
 ## TODO: Profile.
 ## TODO: Add offset as a parameter (which is a parameter of .LOR)?
-## TODO: Add some output, e.g. "Reading files", "Making MTuples", and timing?
+## TODO: Add timing output?
 
 #' Read \code{tsv} output files from \code{comethylation} software.
 #'
@@ -15,16 +15,24 @@
 #' as the \code{seqinfo} argument.
 #'
 #' @param files The \code{.tsv} files created by \code{comethylation}. These 
-#' files may be compressed with gzip or bzip2.
-#' @param sample_names The sample names of each file.
+#' files may be compressed with gzip or bzip2 (not yet implemented). All files 
+#' must contain the same sized m-tuples.
+#' @param sample_names The sample names of each file. Must be unique.
 #' @param methylation_types A character vector with the type of methylation 
-#' type of each file.
+#' type of each file. It is recommended, although not enforced, that all samples 
+#' have the same methylation type.
+#' @param seqinfo A \code{\link[GenomicRanges]{Seqinfo}} object containing 
+#' information about the reference genome of the sample.
+#' @param colData A \code{\link[GenomicRanegs]{DataFrame}} describing the samples. 
+#' Row names, which are required, become the column names of the \code{CoMeth}. 
+#' Must not include a column named \code{methylation_type} andrRow names must 
+#' be identical to \code{sample_names}.
+#' @param exptData An optional \code{\link[IRanges]{SimpleList}} of arbitrary 
+#' content describing the overall experiment.
 #' @param verbose logical Default \code{FALSE}. If \code{TRUE},
 #' \code{read.comethylation()} will report some output when reading the files 
 #' with \code{\link[data.table]{fread}} and may produce some output when 
 #' constructing the \code{CoMeth} object.
-#' @param seqinfo A \code{\link[GenomicRanges]{Seqinfo}} object containing 
-#' information about the reference genome of the sample.
 #' 
 #' @note The compression of \code{file} is determined by the file extension: 
 #' \code{.gz} for compressed with gzip and \code{.bz2} for compressed with 
@@ -36,12 +44,11 @@
 #' cat("TODO")
 #' 
 #' @export
-read.comethylation <- function(files, sample_names, methylation_types, seqinfo,
-                                verbose = FALSE) {
+read.comethylation <- function(files, sample_names, methylation_types, seqinfo, 
+                               colData = DataFrame(), exptData = SimpleList(), 
+                               verbose = FALSE) {
   
-  MAX_VEC_LENGTH <- 2^31 - 1
-  
-  # Check there is a unique sample_name for each filename
+  # Check there is a unique sample name for each filename
   if (length(sample_names) != length(files) | 
         length(sample_names) != length(unique(sample_names))) {
     stop("Each file must have a unique sample name.")
@@ -56,142 +63,120 @@ read.comethylation <- function(files, sample_names, methylation_types, seqinfo,
   if(!.valid_methylation_type(methylation_type)) {
     stop("Invalid methylation type.")
   }
-  colData <- DataFrame(methylation_type = 
-                         rep(methylation_type, length(sample_names)),
-                       row.names = sample_names)
+  if (!identical(dim(colData), c(0L, 0L))) {
+    colData <- DataFrame(methylation_type = 
+                           rep(methylation_type, length(sample_names)), 
+                         row.names = sample_names)
+  } else{
+    if (any(colnames(colData) == 'methylation_type')) {
+      stop(paste0(sQuote('colData'), " must not include a column called ", 
+                  sQuote('methylation_type'), "."))
+    }
+    if (!identical(row.names(colData), sample_names)) {
+      stop(sQuote('row.names'), " of ", sQuote('colData'), " must match ",
+           sQuote('sample_names'), ".")
+    }
+    cbind(colData, DataFrame(methylation_type = 
+                               rep(methylation_type, length(sample_names)), 
+                             row.names = sample_names))
+  }
   
   # Check seqinfo is supplied
   if (missing(seqinfo)){
     stop("Must supply ", sQuote('seqinfo'), '.')
   }
   
-  # Read in files and return as MTuples plus matrix of counts
-  print("Reading files...")
-  x <- bplapply(files, function(file, verbose) {
-    if (grepl("\\.gz$", file)){
-      file <- gunzip(file, remove = FALSE)
-      file_to_remove <- file
-    } else if (grepl("\\.bz2$", file)){
-      stop("Sorry, can't handle ", sQuote('bzip2'), " compressed files.")
-      # TODO: Uncomment once I have a way to deal with bzip files
-      # file <- bzip(file)
-      #file_to_remove <- file
-    } else {
-      # Nothing to do!
-      file_to_remove <- NULL
-    }
-    
-    tsv <- fread(file, header = TRUE, verbose = verbose)
-    # Remove the gunzipped file
-    if (!is.null(file_to_remove)) {
-      unlink(file_to_remove)
-    }
-    
-    # Return as a list of MTuples and counts (as matrix).
-    val <- list(mtuples = MTuples(seqnames = tsv[, chr], strand = tsv[, strand], 
-                                  pos = as.matrix(tsv[, grep('^pos',names(tsv)), 
-                                                      with = FALSE])),
-                counts = c(tsv[, grep('[MU]', names(tsv)), 
-                               with = FALSE])
-    )
-    return(val)
-  }, verbose = verbose)
-  
-  if (length(sample_names) > 1) {
-    print("Combining m-tuples...")
-    # If there are multiple samples/files then need to combine these.
-    # Compare all sample m-tuples (smt) against the sample with the most 
-    # m-tuples (i). Create a list of any putative sample-specific m-tuples and 
-    # unique-ify it (noti; note that m-tuples in noti are not strictly 
-    # sample-specific but are rather just a single copy of all m-tuples that 
-    # are  "not in i"). Then, concatenate m-tuples from sample i with noti 
-    # (mt). By construction, mt will contain a single copy of all m-tuples 
-    # observed in any sample (i.e. no duplicates). This is __much__ faster than 
-    # calling unique(do.call("c", smt)) because most m-tuples are not are not 
-    # sample-specific (i.e. unique) and unique(MTuples) is currently very slow 
-    # if MTuples contains many elements with a large number of duplicates.
-    smt <- lapply(x, '[[', 'mtuples')
-    m <- sapply(smt, getM)
-    if (!.zero_range(m)) {
-      stop("All files must contain the same sized m-tuples.")
-    }
-    m <- m[1L]
-    i <- which.max(sapply(smt, length))
-    # Compare all other samples against i
-    u <- bplapply(seq_along(smt)[-i], function(ii, x, y){
-      Rle(!overlapsAny(x[[ii]], y, type = 'equal'))
-    }, x = smt, y = smt[[i]])
-    noti <- unique(do.call("c", mapply('[', smt[-i], u)))
-    mt <- sort(c(smt[[i]], noti))
-    seqinfo(mt) <- seqinfo
-  
-    # Find overlaps between smt and mt for each sample.
-    ol <- bplapply(smt, function(smt, mt) {
-      findOverlaps(smt, mt, type = 'equal')
-    }, mt = mt)
-  
-    # Construct assays
-    print("Combining assays...")
-    counts <- lapply(x, '[[', 'counts')
-    an <- names(counts[[1]])
-    nmt <- length(mt)
-    
-    ## TODO: Figure out why I can't parallelise this.
-#     if ((nmt * length(counts)) > MAX_VEC_LENGTH) {
-#       cat(paste0("Sorry, cannot run in parallel with matrices containing > ", 
-#             MAX_VEC_LENGTH, " elements.\nReverting to serial processing; this", 
-#             " may take some time as a result..."))
-#       x <- lapply(an, function(an, counts, ol, nmt) {
-#         mat <- matrix(NA_integer_, nrow = nmt, ncol = length(counts))
-#         ri <- unlist(lapply(ol, subjectHits), use.names = FALSE)
-#         ci <- rep(seq_along(counts), times = sapply(ol, queryLength))
-#         mat[ri + (ci - 1) * nrow(mat)] <- unlist(lapply(counts, '[[', an), 
-#                                                  use.names = FALSE)
-#         return(mat)
-#       }, counts = counts, ol = ol, nmt = nmt)
-#     } else {
-#       x <- bplapply(an, function(an, counts, ol, nmt) {
-#         mat <- matrix(NA_integer_, nrow = nmt, ncol = length(counts))
-#         ri <- unlist(lapply(ol, subjectHits), use.names = FALSE)
-#         ci <- rep(seq_along(counts), times = sapply(ol, queryLength))
-#         mat[ri + (ci - 1) * nrow(mat)] <- unlist(lapply(counts, '[[', an), 
-#                                                use.names = FALSE)
-#         return(mat)
-#       }, counts = counts, ol = ol, nmt = nmt)
-#     }
-    x <- lapply(an, function(an, counts, ol, nmt) {
-      mat <- matrix(NA_integer_, nrow = nmt, ncol = length(counts))
-      ri <- unlist(lapply(ol, subjectHits), use.names = FALSE)
-      ci <- rep(seq_along(counts), times = sapply(ol, queryLength))
-      mat[ri + (ci - 1) * nrow(mat)] <- unlist(lapply(counts, '[[', an), 
-                                                   use.names = FALSE)
-      return(mat)
-      }, counts = counts, ol = ol, nmt = nmt)
-
-    names(x) <- an
+  # Decompress files (if required).
+  if (any(grepl("\\.gz$", files) || any(grepl("\\.bz2$", files)))) {
+    print("Decompressing files...")
+    x <- do.call("rbind", bplapply(files, function(file, verbose) {
+      if (grepl("\\.gz$", file)){
+        file <- gunzip(file, remove = FALSE)
+        file_to_remove <- file
+      } else if (grepl("\\.bz2$", file)){
+        stop("Sorry, can't handle ", sQuote('bzip2'), " compressed files.")
+        # TODO: Uncomment once I have a way to deal with bzip files
+        # file <- bzip(file)
+        # file_to_remove <- file
+      } else {
+        # Nothing to do!
+        file_to_remove <- NULL
+      }
+      return(data.frame(file = file, file_to_remove = file_to_remove, 
+                        stringsAsFactors = FALSE))
+    }))
   } else{
-    # Construct rowData
-    mt <- x[[1]][['mtuples']]
-    m <- getM(mt)
-    # Construct assays
-    counts <- x[[1]][['counts']]
-    an <- names(counts)
-    nmt <- length(mt)
-    x <- lapply(counts, as.matrix)
-    names(x) <- an
-  }
+    # No files require decompressing
+    x <- data.frame(file = file, file_to_remove = rep(NULL, length(files)))
+  } 
   
+  # Read in file(s) serially and, if more than one file, merge these files.
+  if (length(files) == 1L) {
+    print(paste0("Reading file ", files[1], " ..."))
+  } else {
+    print(paste0("Reading file ", files[1], " ..."))
+  }
+  mtsv <- fread(input = x$file[1], header = TRUE, verbose = verbose)
+  keys <- c("chr", "strand", 
+            grep(pattern = '^pos', x = names(mtsv), value = TRUE))
+  setkeyv(x = mtsv, cols = keys, verbose = verbose)
+  # Append the sample name to the names of the counts, e.g. 'M' -> 'M.sample1'
+  wn <- which(!(names(mtsv) %in% keys))
+  setnames(mtsv, c(names(mtsv)[seq_len(ncol(mtsv))[-wn]], 
+                   paste(names(mtsv)[wn], sample_names[1], sep = ".")))
+  rm(wn)
+  if (length(files) > 1) {
+    for (i in seq.int(from = 2, to = length(files), by = 1)) {
+      print(paste0("Reading and merging ", files[i], ' ...'))
+      ## TODO: try-catch the merge in case files with different sized m-tuples
+      ## are supplied by the user because the error message supplied by 
+      ## merge is a bit hard to understand.
+      tmp <- fread(input = x$file[i], header = TRUE, verbose = verbose)
+      setkeyv(x = tmp, cols = keys, verbose = verbose)
+      wn <- which(!(names(tmp) %in% keys))
+      setnames(tmp, c(names(tmp)[seq_len(ncol(tmp))[-wn]], 
+                      paste(names(tmp)[wn], sample_names[i], sep = ".")))
+      mtsv <- merge(mtsv, tmp, by = keys, all = TRUE)
+      rm(tmp, wn)
+    }
+  }
+  # Remove the decompressed files (if required)
+  unlink(x$file_to_remove)
+  
+  # Combine the counts into the assays list
+  print("Creating assays...")
+  assay_names <- unique(sapply(strsplit(grep(pattern = '\\.', x = names(mtsv), 
+                                             value = TRUE),
+                                        split = '\\.'), '[[', 1))
+  assays <- lapply(assay_names, function(an, mtsv) {
+    i <- grep(pattern = an, x = names(mtsv), value = TRUE)
+    sn <- sapply(strsplit(x = i, split = '\\.'), '[[', 2)
+    as.matrix(setnames(mtsv[, i, with = FALSE], sn))
+  }, mtsv = mtsv)
+  names(assays) <- assay_names
+  
+  rowData <- MTuples(seqnames = mtsv[, chr], strand = mtsv[, strand], 
+                     pos = as.matrix(mtsv[, grep(pattern = '^pos', 
+                                                 x = colnames(mtsv)), 
+                                          with = FALSE]), 
+                     seqinfo = seqinfo)
+  rm(mtsv)
+  
+  m <- getM(rowData)
   if (m == 1L) {
     class <- 'CoMeth1'
-    assays <- c(x, list(EP = .EP(x), beta = .beta(x)))
+    assays <- c(assays, list(beta = .beta(assays)))
   } else if (m == 2L) {
-    assays <- c(x, list(EP = .EP(x), LOR = .LOR(x)))
     class <- 'CoMeth2'
+    assays <- c(assays, list(LOR = .LOR(assays)))
   } else{
-    assays <- c(x, list(EP = .EP(x)))
     class <- 'CoMeth3Plus'
+    # No need to update assays
   }
+  
+  # Create the CoMeth object
   print("Creating CoMeth object...")
-  new(class, SummarizedExperiment(assays = assays, rowData = mt, 
-                                  colData = colData, verbose = verbose))
+  new(class, SummarizedExperiment(assays = assays, rowData = rowData, 
+                                  colData = colData, exptData = exptData, 
+                                  verbose = verbose))
 }
